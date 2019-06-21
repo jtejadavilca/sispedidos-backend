d56e0e49dc2447358a7dfd3d7103f73a37440505
José Tejada <jtejadav@canvia.com>
2019-06-21 13:22:18 -0500

    Proyecto pedidos backend inicial

---
+++ src/main/java/pe/com/confisys/soft/sispedidosbackend/SispedidosBackendApplication.java
@@ -0,0 +1,13 @@
+package pe.com.confisys.soft.sispedidosbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SispedidosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SispedidosBackendApplication.class, args);
+	}
+
+}
