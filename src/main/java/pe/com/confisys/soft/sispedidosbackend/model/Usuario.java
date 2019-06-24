cfded33bf3c6978ce3110015f3c27308941848a1
José Tejada <jtejadav@canvia.com>
2019-06-24 10:18:07 -0500

    Se crea clase de entidad Usuario

---
+++ src/main/java/pe/com/confisys/soft/sispedidosbackend/model/Usuario.java
@@ -0,0 +1,17 @@
+package pe.com.confisys.soft.sispedidosbackend.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter @NoArgsConstructor
+public class Usuario implements Serializable{
+
+	private String username;
+	private String password;
+}
