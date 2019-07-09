5c9989236ecfb5ac52628f4b0b3dda51b6c7e1f7
Jose Tejada <jtejadavilca@gmail.com>
2019-07-08 23:39:59 -0500

    Se crea PedidosServiceException para manejar errores propios del negocio

---
+++ src/main/java/pe/com/confisys/soft/sispedidosbackend/exceptions/PedidosServiceException.java
@@ -0,0 +1,12 @@
+package pe.com.confisys.soft.sispedidosbackend.exceptions;
+
+public class PedidosServiceException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7895209854921025979L;
+
+	public PedidosServiceException(String msj) {
+		super(msj);
+	}
+}
